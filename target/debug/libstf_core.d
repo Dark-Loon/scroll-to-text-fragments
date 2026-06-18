adea9827ee48d260ff2e092cce03f0ac0c6d2035
Dark_Loon <armandgaeln@gmail.com>
2026-06-18 13:25:17 +0200

    Remove target directory from tracking

--- target/debug/libstf_core.d
+++
@@ -1,1 +0,0 @@
-/home/armand/rust/scroll_to_text_fragments/target/debug/libstf_core.rlib: /home/armand/rust/scroll_to_text_fragments/stf_core/src/lib.rs
--- target/debug/stf.d
+++
@@ -1,1 +0,0 @@
-/home/armand/rust/scroll_to_text_fragments/target/debug/stf: /home/armand/rust/scroll_to_text_fragments/stf_cli/src/main.rs
